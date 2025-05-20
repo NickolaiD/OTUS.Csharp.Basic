c51da6962f594c9aaaa069ef63504a22b53cdaf7
Дроздов Николай <nickolai.drozdow@gmail.com>
2025-05-20 22:03:44 +0300

    HomeWork02

---
+++ HomeWork/HomeWork02/TelegramBot/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TelegramBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userName = "";
+            while (true) {
+                var userCommand = Console.ReadLine();
+
+                switch (userCommand)
+                {
+                    case "/start":
+                        Console.Write("Введите свое имя: ");
+                        userName = Console.ReadLine();
+                        break;
+
+                    case "/help":
+                        Console.WriteLine("Справочная информация:");
+                        Console.WriteLine("/start - начало работы");
+                        Console.WriteLine("/help - справочная информация");
+                        Console.WriteLine("/info - информация о версии программы и дате её создания");
+                        Console.WriteLine("/echo - программа возвращает введенный текст (например, /echo Hello)");
+                        Console.WriteLine("/exit - завершение работы");
+                        break;
+
+                    case "/info":
+                        ;
+                        break;
+
+                    case "/echo":
+                        ;
+                        break;
+
+                    case "/exit": 
+                        break;
+
+                    default:
+                        Console.WriteLine("Команда не существует");
+                        break;
+                }
+
+            }
+        }
+
+        private string GetUserName(string userName)
+        {
+            return userName != "" ? userName : "Пользователь";
+        }
+    }
+}
