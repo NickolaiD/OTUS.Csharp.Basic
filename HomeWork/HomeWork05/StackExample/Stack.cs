fdcc5fe12c68021688bfc4ae64d09586747ced24
Дроздов Николай <nickolai.drozdow@gmail.com>
2025-06-17 21:10:31 +0300

    HomeWork05 - base task

---
+++ HomeWork/HomeWork05/StackExample/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+
+namespace StackExample
+{
+    class Stack
+    {
+        private List<string> _stackList;
+        public int Size { get => _stackList.Count; }
+        public string? Top 
+        { 
+            get
+            {
+                string? result = null;
+                if (_stackList.Count > 0)
+                {
+                    result = _stackList[_stackList.Count - 1];
+                }
+                return result;
+            } 
+        }
+
+        public Stack(params string[] stackValues)
+        {
+            _stackList = new List<string>();
+            foreach (string stackValue in stackValues)
+            {
+                Add(stackValue);
+            }
+
+        }
+
+        public void Add(string stackValue)
+        {
+            _stackList.Add(stackValue);
+        }
+
+        public string Pop()
+        {
+            if (_stackList.Count == 0)
+                throw new Exception("Стек пустой");
+
+            var result = _stackList[_stackList.Count - 1];
+            _stackList.RemoveAt(_stackList.Count - 1);
+            return result;
+        }
+    }
+}
