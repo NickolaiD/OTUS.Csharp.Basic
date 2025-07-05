4b8730832097b27b3a9a90ffcf73af40ec0a084b
Дроздов Николай <nickolai.drozdow@gmail.com>
2025-07-05 15:17:01 +0300

    HomeWork05-2 Subtask4

---
+++ HomeWork/HomeWork05-2/TelegramBot/TelegramBot/UserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    internal class UserService : IUserService
+    {
+        public ToDoUser? GetUser(long telegramUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ToDoUser RegisterUser(long telegramUserId, string telegramUserName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
