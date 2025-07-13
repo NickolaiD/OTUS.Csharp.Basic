291b920f7a0c7cdc4788697739128569f066fcb3
Дроздов Николай <nickolai.drozdow@gmail.com>
2025-07-13 13:26:28 +0300

    Home work 06 - Init

---
+++ HomeWork/HomeWork06/TelegramBot/TelegramBot/Program.cs
@@ -0,0 +1,36 @@
+﻿using Otus.ToDoList.ConsoleBot;
+using Otus.ToDoList.ConsoleBot.Types;
+
+namespace TelegramBot
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+
+                Console.WriteLine("Введите максимально допустимое количество задач");
+                var taskCountLimit = UpdateHandler.ParseAndValidateInt(Console.ReadLine(), 1, 100);
+
+                Console.WriteLine("Введите максимально допустимую длину задачи");
+                var taskLengthLimit = UpdateHandler.ParseAndValidateInt(Console.ReadLine(), 1, 100);
+
+                var botClient = new ConsoleBotClient();
+                var handler = new UpdateHandler(new UserService(), botClient, new ToDoService(botClient, taskCountLimit, taskLengthLimit));
+            
+                botClient.StartReceiving(handler);
+            }
+            catch (Exception ex)
+            {
+                ShowError($"Произошла непредвиденная ошибка:{ex.GetType()}\n{ex.Message}\n{ex.StackTrace}\n{ex.InnerException}");
+            }
+        }
+        private static void ShowError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ HomeWork/HomeWork06/TelegramBot/TelegramBot/ToDoItem.cs
@@ -0,0 +1,22 @@
+﻿namespace TelegramBot
+{
+    internal class ToDoItem
+    {
+        public Guid Id { get; }
+        public ToDoUser User { get; }
+        public string Name { get; }
+        public DateTime CreatedAt { get; }
+        public ToDoItemState State { get; set; }
+        public DateTime? StateChangedAt { get; set; }
+
+        public ToDoItem(ToDoUser user, string name)
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+            State = ToDoItemState.Active;
+            User = user;
+            Name = name;
+        }
+
+    }
+}
