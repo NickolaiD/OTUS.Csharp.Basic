3612a562ebe63f5ba16a490dad7aaec73aadf5b5
Дроздов Николай <nickolai.drozdov@gmail.com>
2025-09-13 22:41:58 +0300

    HomeWork12 - Fix

--- HomeWork/HomeWork12/TelegramBot/TelegramBot/Helpers/EnumerableExtension.cs
+++ HomeWork/HomeWork12/TelegramBot/TelegramBot/Helpers/EnumerableExtension.cs
@@ -10,30 +10,13 @@ namespace TelegramBot.Helpers
     {
         public static IEnumerable<T> GetBatchByNumber<T>(this IEnumerable<T> value, int batchSize, int batchNumber)
         {
-            try
-            {
-                //return value.Chunk(batchSize).ToArray()[batchNumber];
-                
-                if (value == null)
-                    throw new ArgumentNullException(nameof(value));
+            if (batchSize <= 0)
+                throw new ArgumentException("Размер пачки должен быть больше 0");
 
-                if (batchSize <= 0)
-                    throw new ArgumentException("Размер пачки должен быть больше 0", nameof(batchSize));
+            if (batchNumber <= 0)
+                throw new ArgumentException("Номер пачки должен быть больше 0");
 
-                if (batchNumber < 0)
-                    throw new ArgumentException("Номер пачки не может быть отрицательным", nameof(batchNumber));
-
-
-                return value
-                    .Skip(batchNumber * batchSize)
-                    .Take(batchSize);
-
-            }
-            catch (Exception ex) 
-            {
-                //return Enumerable.Empty<T>();
-                throw new ArgumentException("Неверные аргументы для текущей коллекции");
-            }
+            return value.Skip(batchNumber * batchSize).Take(batchSize);
         }
     }
 }
