592e4f372eebd89f543bffe9327236984c821438
Дроздов Николай <nickolai.drozdow@gmail.com>
2025-10-07 21:19:43 +0300

    HomeWork14 - SubTask1

--- HomeWork/HomeWork14/TelegramBot/TelegramBot/Entities/ToDoUser.cs
+++ HomeWork/HomeWork14/TelegramBot/TelegramBot/Entities/ToDoUser.cs
@@ -2,18 +2,9 @@
 {
     public class ToDoUser
     {
-        public Guid UserId { get; init; }
-        public string TelegramUserName { get; init; }
-        public DateTime RegisteredAt { get; init; }
-        public long TelegramUserId { get; init; }
-        public ToDoUser(string telegramUserName, long telegramUserId)
-        {
-            UserId = Guid.NewGuid();
-            RegisteredAt = DateTime.UtcNow;
-            TelegramUserName = telegramUserName;
-            TelegramUserId = telegramUserId;
-        }
-        public ToDoUser() { }
-
+        public Guid UserId { get; set; }
+        public string TelegramUserName { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        public long TelegramUserId { get; set; }
     }
 }
