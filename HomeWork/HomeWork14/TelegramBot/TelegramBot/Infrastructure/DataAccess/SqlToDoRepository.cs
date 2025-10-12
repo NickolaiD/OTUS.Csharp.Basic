4c0f92fe6110635bb101e4d1a60f40b74fbc24d5
Дроздов Николай <nickolai.drozdov@gmail.com>
2025-10-12 17:50:23 +0300

    HomeWork14 - SubTask4

---
+++ HomeWork/HomeWork14/TelegramBot/TelegramBot/Infrastructure/DataAccess/SqlToDoRepository.cs
@@ -0,0 +1,47 @@
+﻿using LinqToDB.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBot.Core.DataAccess;
+using TelegramBot.Entities;
+
+namespace TelegramBot.Infrastructure.DataAccess
+{
+    internal class SqlToDoRepository : IToDoListRepository
+    {
+        private IDataContextFactory<DataConnection> _dataContext;
+
+        public SqlToDoRepository(IDataContextFactory<DataConnection> dataContext)
+        {
+            _dataContext = dataContext;
+            _dataContext.
+        }
+
+        public Task Add(ToDoList list, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Guid id, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsByName(Guid userId, string name, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ToDoList?> Get(Guid id, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<ToDoList>> GetByUserId(Guid userId, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
